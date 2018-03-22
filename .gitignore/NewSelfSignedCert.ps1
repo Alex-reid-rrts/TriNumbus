16b119b16806d4994db31c0e598bf5925250322c
Alex-reid-rrts <33097791+Alex-reid-rrts@users.noreply.github.com>
2018-03-21 21:06:31 -0600

    Delete NewSelfSignedCert.ps1

--- .gitignore/NewSelfSignedCert.ps1
+++
@@ -1,7 +0,0 @@
-New-SelfSignedCertificate -DnsName "trinimbus.rrts.tech" -CertStoreLocation "cert:\LocalMachine\My"
-$SSLCert = Get-ChildItem -path cert:\LocalMachine\my | Select Thumbprint;
-$Thumbprint = $SSLCert.Thumbprint;
-$webServerCert = get-item Cert:\LocalMachine\My\$Thumbprint;
-New-WebBinding -Name "Default Web Site" -IPAddress * -Port 443 -Protocol "https"
-$bind = Get-WebBinding -Name "Default Web Site"-Protocol https;
-$bind.AddSslCertificate($webServerCert.GetCertHashString(), "my")
